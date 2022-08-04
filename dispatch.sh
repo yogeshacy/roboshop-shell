f4b686051a2867f16da00e7b34a6b029c2c47c4e
yogesh <yogeshacy@outlook.com>
2022-08-04 23:27:59 +0530

    Add all component script 02

--- dispatch.sh
+++ dispatch.sh
@@ -3,6 +3,8 @@ yum install golang -y
 useradd roboshop
 
 curl -L -s -o /tmp/dispatch.zip https://github.com/roboshop-devops-project/dispatch/archive/refs/heads/main.zip
+#cd /home/dispatch
+#rm -rf dispatch
 unzip -o /tmp/dispatch.zip
 mv dispatch-main dispatch
 cd dispatch
--- user.sh
+++ user.sh
@@ -5,6 +5,7 @@ useradd roboshop
 
 curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
 cd /home/roboshop
+rm -rf user
 unzip /tmp/user.zip
 mv user-main user
 cd /home/roboshop/user
